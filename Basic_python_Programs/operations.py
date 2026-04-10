a00d0afbd603a29e0abb30e120242e21cd520848
shreyasshuklaa <shreyashshukla194@gmail.com>
2026-04-11 01:42:13 +0530

    Opeartions of arithmetic, Comaparison,Logical

---
+++ Basic_python_Programs/operations.py
@@ -0,0 +1,19 @@
+a = 10
+b = 5
+
+# Arithmetic operations
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)   
+
+# Comparison operations
+print("a > b:", a > b)
+print("a < b:", a < b)
+print("a == b:", a == b)   
+print("a != b:", a != b)   
+
+# Logical operations
+print((a > 5 and b < 10))   
+print((a > 5 or b > 10))    
+print(not(a > b))           
