bea608e61473e968b0f941769d5a991876d145f3
Shreyash Shukla <shreyashshukla194@gmail.com>
2026-04-12 19:52:02 +0530

    Add break and continue examples in break_continue.py
    Demonstrate usage of break and continue in loops.

---
+++ Basic_python_Programs/break_continue.py
@@ -0,0 +1,13 @@
+# Using break
+
+for i in range(1, 6):
+    if i == 4:
+        break   # loop stops at 4
+    print(i)
+
+# Using continue
+
+for i in range(1, 6):
+    if i == 3:
+        continue   # skip 3
+    print(i)
