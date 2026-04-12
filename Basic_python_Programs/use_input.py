609b8af81f99ee9c4919a3a984130f2e25395681
shreyasshuklaa <shreyashshukla194@gmail.com>
2026-04-12 19:24:15 +0530

    By input lower upper and split replace in python

---
+++ Basic_python_Programs/use_input.py
@@ -0,0 +1,18 @@
+text = input("Enter a String: ")
+
+# Concatenation
+String_Concat = text + " is Awesome"
+print(String_Concat)
+
+# Indexing
+print(text[0])   # first character
+
+# String methods
+print(String_Concat.lower())   
+print(String_Concat.upper())
+
+print(String_Concat.split())   
+
+print(String_Concat.replace("a", "@"))   
+
+print(len(String_Concat))   
