de8d06ec28568c1201dec4886e3a822162e75c74
Shreyash Shukla <shreyashshukla194@gmail.com>
2026-04-12 19:45:03 +0530

    Implement for and while loop examples
    Add examples of for and while loops in Python.

---
+++ Basic_python_Programs/loop.py
@@ -0,0 +1,11 @@
+# Using for loop
+
+for i in range(1, 11):   # 1 to 10
+    print(i)
+
+# Using while loop
+
+i = 1
+while i <= 10:
+    print(i)
+    i += 1   # increment (important, warna infinite loop)
